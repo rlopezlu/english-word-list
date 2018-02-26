bb076726f95168424cf682fa63cc7b2caa79e48f
Raul Lopez Luna <raullopez2094@gmail.com>
2018-02-26 11:29:37 -0800

    more functions added

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 var myArray = require('./arrayOfWords.js').myArray;
 
-exports.minLength = function(minLength){
+module.exports.minLength = function(minLength){
   return myArray.filter(word =>{
     return word.length >= minLength
   })
 }
 
-exports.maxLength = function(maxLength){
+module.exports.maxLength = function(maxLength){
   return myArray.filter(word =>{
-    return word.length <= maxLength
+    return word.length <= maxLengthn
   })
 }
 
-exports.range = function(min, max){
+module.exports.range = function(min, max){
   return myArray.filter(word => {
     return word.length >= min && word.length <= max
   })
 }
 
-exports.amount = function(count){
+module.exports.amount = function(count){
   return myArray.filter(word => {
     return word.length > 3
   }).splice(1, count)
+}
+
+module.exports.randomize = function(list){
+  return list.sort( (a,b) =>{
+    return 0.5 - Math.random()
+  })
 }
