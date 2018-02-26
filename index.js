9b44762294482a878c7c9bdfc3e19381ec5ea1b1
Raul Lopez Luna <raullopez2094@gmail.com>
2018-02-26 11:37:46 -0800

    removed es6 arrow functions

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 var myArray = require('./arrayOfWords.js').myArray;
 
 module.exports.minLength = function(minLength){
-  return myArray.filter(word =>{
+  return myArray.filter(function(word){
     return word.length >= minLength
   })
 }
 
 module.exports.maxLength = function(maxLength){
-  return myArray.filter(word =>{
+  return myArray.filter(function(word){
     return word.length <= maxLengthn
   })
 }
 
 module.exports.range = function(min, max){
-  return myArray.filter(word => {
+  return myArray.filter(function(word){
     return word.length >= min && word.length <= max
   })
 }
 
 module.exports.amount = function(count){
-  return myArray.filter(word => {
+  return myArray.filter(function(word){
     return word.length > 3
   }).splice(1, count)
 }
 
 module.exports.randomize = function(list){
-  return list.sort( (a,b) =>{
+  return list.sort( function(a,b){
     return 0.5 - Math.random()
   })
 }
